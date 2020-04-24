b98b252d967c2834ece9ed27891599df86684e84
Maximilian Letter <maximilian.letter@gmail.com>
2020-04-24 10:47:39 +0200

    Build rectangles out of vertical lines

--- door-detection-c++.cpp
+++ door-detection-c++.cpp
@@ -14,25 +14,38 @@ const int RES = 180;
 
 const float CONTRAST = 1.5;
 
+// Blur constants
 const Size BLUR_KERNEL = Size(3, 3);
 const float BLUR_SIGMA = 2.5;
 
+// Canny constants
 const int CANNY_LOWER = 50;
 const int CANNY_UPPER = 200;
 
 // NOTE: these values need to be improved to ensure to always find the corners of a door
+// Corner detection constants
 const int CORNERS_MAX = 40;
 const float CORNERS_QUALITY = 0.05;
 const float CORNERS_MIN_DIST = 10.0;
 const int CORNERS_MASK_OFFSET = 10;
 
+// Vertical lines constants
 const float LINE_MAX = 0.85;
 const float LINE_MIN = 0.3;
-const float LINE_ANGLE_MIN = 0.875;
+const float LINE_ANGLE_MIN = 0.875; // RAD
+
+// Rectangles constants
+const float ANGLE_MAX = 0.175; // RAD
+const float LENGTH_DIFF_MAX = 0.15;
+const float ASPECT_RATIO_MIN = 0.35;
+const float ASPECT_RATIO_MAX = 0.7;
+const float LENGTH_HOR_DIFF_MAX = 1.1;
+const float LENGTH_HOR_DIFF_MIN = 0.7;
 
 // Declare all used functions
 bool detect(Mat image);
-vector<vector<Point2f>> cornersToVertLines(vector<Point2f> corners, double height);
+vector<vector<Point2f>> cornersToVertLines(vector<Point2f> corners, int height);
+vector<vector<Point2f>> vertLinesToRectangles(vector<vector<Point2f>> lines);
 
 float getDistance(Point2f p1, Point2f p2);
 float getOrientation(Point2f p1, Point2f p2);
@@ -101,33 +114,45 @@ bool detect(Mat image)
 	goodFeaturesToTrack(blurred, corners, CORNERS_MAX, CORNERS_QUALITY, CORNERS_MIN_DIST, mask);
 
 	// Connect corners to vertical lines
-	vector<vector<Point2f>> lines = cornersToVertLines(corners, RES * ratio);
-	
+	vector<vector<Point2f>> lines = cornersToVertLines(corners, int(RES * ratio));
+
+	// Group corners based on found lines to rectangles
+	vector<vector<Point2f>> rectangles = vertLinesToRectangles(lines);
 
 	// Display results
 
-	/*cout << corners.size();
+	cout << corners.size();
 	for (int i = 0; i < corners.size(); i++)
 	{
 		circle(image, corners[i], 3, Scalar(0, 255, 0), FILLED);
-	}*/
-
-	cout << lines.size();
-	for (int i = 0; i < lines.size(); i++)
-	{
-		line(image, lines[i][0], lines[i][1], Scalar(0, 0, 255), 1);
 	}
+
+	//cout << lines.size();
+	//for (int i = 0; i < lines.size(); i++)
+	//{
+	//	line(image, lines[i][0], lines[i][1], Scalar(0, 0, 255), 1);
+	//}
+
+	cout << rectangles.size();
+	for (int i = 0; i < rectangles.size(); i++)
+	{
+		/*polylines(image, rectangles[i], true, Scalar(255, 255, 0), 1);*/
+		line(image, rectangles[i][0], rectangles[i][1], Scalar(255, 255, 0), 1);
+		line(image, rectangles[i][1], rectangles[i][2], Scalar(255, 255, 0), 1);
+		line(image, rectangles[i][2], rectangles[i][3], Scalar(255, 255, 0), 1);
+		line(image, rectangles[i][3], rectangles[i][0], Scalar(255, 255, 0), 1);
+	}
+
 	imshow("Display window", image);
-	waitKey(0);
 
 	return true;
 }
 
 // Group corners to vertical lines that represent the door posts. 
-vector<vector<Point2f>> cornersToVertLines(vector<Point2f> corners, double height)
+vector<vector<Point2f>> cornersToVertLines(vector<Point2f> corners, int height)
 {
-	double lengthMax = LINE_MAX * height;
-	double lengthMin = LINE_MIN * height;
+	float lengthMax = LINE_MAX * height;
+	float lengthMin = LINE_MIN * height;
 
 	vector<vector<Point2f>> lines;
 	vector<bool> done;
@@ -136,7 +161,7 @@ vector<vector<Point2f>> cornersToVertLines(vector<Point2f> corners, double heigh
 	{
 		for (int j = 0; j < corners.size(); j++)
 		{
-			if (j >= i) continue;
+			if (j <= i) continue;
 
 			float distance = getDistance(corners[i], corners[j]);
 			if (distance < lengthMin || distance > lengthMax)
@@ -150,12 +175,90 @@ vector<vector<Point2f>> cornersToVertLines(vector<Point2f> corners, double heigh
 				continue;
 			}
 
-			vector<Point2f> line = { corners[i], corners[j] };
+			// Sort by y-value, so that the high points are first
+			vector<Point2f> line;
+			if (corners[i].y < corners[j].y)
+			{
+				line = { corners[i], corners[j] };
+			}
+			else {
+				line = { corners[j], corners[i] };
+			}
 			lines.push_back(line);
 		}
 	}
 
 	return lines;
+}
+
+vector<vector<Point2f>> vertLinesToRectangles(vector<vector<Point2f>> lines)
+{
+	vector<vector<Point2f>> rects;
+
+	for (int i = 0; i < lines.size(); i++)
+	{
+		for (int j = 0; j < lines.size(); j++)
+		{
+			if (j <= i) continue;
+
+			// Only build rectangle if the two lines are completely distinct
+			if ((lines[i][0] == lines[j][0]) || (lines[i][0] == lines[j][1]) || (lines[i][1] == lines[j][0]) || (lines[i][1] == lines[j][1]))
+			{
+				continue;
+			}
+
+			// NOTE: both of these values was calculated before,
+			// maybe store them for reusage
+			// Check if length difference of lines is close
+			float length1 = getDistance(lines[i][0], lines[i][1]);
+			float length2 = getDistance(lines[i][0], lines[i][1]);
+			float lengthDiff = abs(length1 - length2);
+			float lengthAvg = (length1 + length2) / 2;
+
+			if ((lengthDiff > (lengthAvg * LENGTH_DIFF_MAX)))
+			{
+				continue;
+			}
+
+			// Check if top distance is in range of the given aspect ratio
+			float lengthMin = lengthAvg * ASPECT_RATIO_MIN;
+			float lengthMax = lengthAvg * ASPECT_RATIO_MAX;
+
+			float distanceTop = getDistance(lines[i][0], lines[j][0]);
+			if (distanceTop < lengthMin || distanceTop > lengthMax)
+			{
+				continue;
+			}
+
+			// Check if bottom distance is similar to top distance
+			float distanceBot = getDistance(lines[i][1], lines[j][1]);
+			if (distanceBot > (distanceTop * LENGTH_HOR_DIFF_MAX)
+				|| distanceBot < (distanceTop * LENGTH_HOR_DIFF_MIN))
+			{
+				continue;
+			}
+
+			// Test orientation of top horizontal line
+			float orientationTop = getOrientation(lines[i][0], lines[j][0]);
+			if (orientationTop > ANGLE_MAX)
+			{
+				continue;
+			}
+
+			// Test orientation of bottom horizontal line
+			float orientationBot = getOrientation(lines[i][0], lines[j][0]);
+			if (orientationBot > ANGLE_MAX)
+			{
+				continue;
+			}
+
+			// Sort in order: leftBot > leftTop > rightTop > rightBot
+			vector<Point2f> group = { lines[i][1], lines[i][0], lines[j][0], lines[j][1] };
+			rects.push_back(group);
+		}
+	}
+
+	return rects;
 }
 
 // Get the distance between two points
